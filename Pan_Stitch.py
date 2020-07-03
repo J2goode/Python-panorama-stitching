96510c24b26722c30c7e012e216fa4c1621eaf5b
J2goode <67770226+J2goode@users.noreply.github.com>
2020-07-03 15:37:19 -0700

    Update Pan_Stitch.py

--- Pan_Stitch.py
+++ Pan_Stitch.py
@@ -1,5 +1,21 @@
 ## FIND THE PICTURES TO STITCH TOGETHER IN THE FOLDER PROGRAM'S IN ##
 
+# list of things to import
+
+# get the current path of the folder
+
+# make a list of each picture
+
+
 ## STITCH THE PICS TOGETHER ##
 
+# find similarities in pics
+
+# if they dont have any, say they dont and end the program
+
+# if they do, merge them together
+
+
 ## SAVE THE PICS TO THE FOLDER ##
+
+# self explanatory
